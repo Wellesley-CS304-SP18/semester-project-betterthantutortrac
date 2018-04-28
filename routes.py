4171ffe4ac3ff6bbb0cb8161938dab136daed452
Angelina Li <ali6@wellesley.edu>
2018-04-28 19:33:45 -0400

    started login route

--- routes.py
+++ routes.py
@@ -5,8 +5,7 @@ last modified: 04/28/2018
 description: routes for app
 """
 
-from flask import render_template
-
+from flask import render_template, flash, request, redirect, url_for, session
 from app import app
 
 @app.route("/", methods=["GET"])
@@ -14,6 +13,20 @@ from app import app
 def index():
     params = {"title": "Home"}
     return render_template("index.html", **params)
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    # edit the following to make it work
+    params = {"title": "Login"}
+    if "userid" in session:
+        flash("Already logged in!")
+        return redirect(url_for("index"))
+    if request.method == "POST":
+        email = request.form.get("email")
+        pwd = request.form.get("password")
+        session["userid"] = 1  # fill this in
+        flash("Login successful")
+    return render_template("login.html", **params)
 
 @app.route("/test/", methods=["GET"])
 def test():
